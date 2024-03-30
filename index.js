037e130e498b93584c8fcc5c28a6c2edaeec24a2
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-03-30 15:15:56 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 main.remove();
+const body = document.createElement('body')
 const newHeader = document.createElement("H1");
 newHeader.id = "victory";
 newHeader.textContent = "Daniel is the champion"
+document.body.append(newHeader);
